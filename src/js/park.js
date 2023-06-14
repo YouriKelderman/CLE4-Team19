a74eb468eee2e05f927949b4c33a394e81070ca5
SijmenL <lokerssijmen@gmail.com>
2023-06-14 14:30:49 +0200

    map

--- src/js/park.js
+++ src/js/park.js
@@ -1,12 +1,13 @@
 import {Actor, Engine, Vector, Color, Debug, Physics, Input, Axis, CollisionType, Shape} from "excalibur";
 import {Scene} from "excalibur";
-import {Placeholder} from "./placeholder.js"
+import {Placeholder} from "./placeholder.js";
 import {Resources, ResourceLoader} from "./resources.js";
 
 
 let placing = false;
 let placingSprite;
-let int =0;
+let int = 0;
+
 export class Park extends Scene {
 
     constructor() {
@@ -16,40 +17,42 @@ export class Park extends Scene {
 
     onInitialize(_engine) {
         placingSprite = new Placeholder();
-        this.engine.input.pointers.primary.on("down", () => this.mouseInput())
+        this.engine.input.pointers.primary.on("down", () => this.mouseInput());
 
         let mapFloor = new Actor();
         mapFloor.graphics.use(Resources.Map1Ground.toSprite());
-        mapFloor.scale = new Vector(12, 12)
-        mapFloor.pos = new Vector(this.engine.canvasWidth / 5, this.engine.canvasHeight / 5)
-        this.add(mapFloor)
+        mapFloor.scale = new Vector(12, 12);
+        mapFloor.pos = new Vector(775, 335);
+        this.add(mapFloor);
     }
-mouseInput(){
-        if(placing){
+
+    mouseInput() {
+        if (placing) {
             let newClone = new Placeholder();
             newClone.pos = placingSprite.pos;
             this.add(newClone);
-            newClone.checkSelf(int)
+            newClone.checkSelf(int);
         }
-}
+    }
+
     onPreUpdate(engine, delta) {
 
         if (engine.input.keyboard.wasPressed(Input.Keys.B)) {
             placing = !placing;
-            console.log(int)
+            console.log(int);
             if (placing) {
                 this.add(placingSprite);
-                placingSprite.checkSelf(int)
+                placingSprite.checkSelf(int);
             } else {
                 placingSprite.kill();
             }
         }
         if (engine.input.keyboard.wasPressed(Input.Keys.N)) {
             int += 1;
-            if(int > 1) {
+            if (int > 1) {
                 int = 0;
             }
-            placingSprite.checkSelf(int)
+            placingSprite.checkSelf(int);
         }
         if (placing) {
             placingSprite.pos = engine.input.pointers.primary.lastWorldPos;
