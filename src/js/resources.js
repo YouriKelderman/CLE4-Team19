cdc08432f3893e7628777adc31a77492771efd5b
SijmenL <lokerssijmen@gmail.com>
2023-06-14 14:10:05 +0200

    logo

--- src/js/resources.js
+++ src/js/resources.js
@@ -3,17 +3,20 @@ import logo from '../images/logo.png';
 import start from '../images/start.png';
 import rock from '../images/rock.png';
 import kevin from '../images/kevin.png';
+import map1ground from '../images/map-1-gorund.png'
 const Resources = {
     Logo: new ImageSource(logo),
     Start: new ImageSource(start),
     Rock: new ImageSource(rock),
 Kevin: new ImageSource(kevin),
+    Map1Ground: new ImageSource(map1ground),
 };
 const ResourceLoader = new Loader([
     Resources.Logo,
     Resources.Start,
     Resources.Rock,
     Resources.Kevin,
+    Resources.Map1Ground,
 ]);
 
 export {Resources, ResourceLoader};
