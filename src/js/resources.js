a6d573649634beca0e2847d0dc10d7be7d57e703
SijmenL <lokerssijmen@gmail.com>
2023-06-14 11:42:38 +0200

    menu

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,15 +1,18 @@
 import {ImageSource, Sound, Resource, Loader} from 'excalibur';
 import logo from '../images/logo.png';
 import start from '../images/start.png';
+import rock from '../images/rock.png';
 
 const Resources = {
     Logo: new ImageSource(logo),
     Start: new ImageSource(start),
+    Rock: new ImageSource(rock),
 
 };
 const ResourceLoader = new Loader([
     Resources.Logo,
-    Resources.Start
+    Resources.Start,
+    Resources.Rock
 ]);
 
 export {Resources, ResourceLoader};
