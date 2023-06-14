15edcc919450a6ae3145efc47b0e40af7d06a46d
YouriKelderman <113994374+YouriKelderman@users.noreply.github.com>
2023-06-14 10:33:03 +0200

    Initial commit

---
+++ src/js/game.js
@@ -0,0 +1,22 @@
+import '../css/style.css'
+import { Actor, Engine, Vector } from "excalibur"
+import { Resources, ResourceLoader } from './resources.js'
+
+export class Game extends Engine {
+
+    constructor() {
+        super({ width: 800, height: 600 })
+        this.start(ResourceLoader).then(() => this.startGame())
+    }
+
+    startGame() {
+        console.log("start de game!")
+        const fish = new Actor()
+        fish.graphics.use(Resources.Fish.toSprite())
+        fish.pos = new Vector(400, 300)
+        fish.vel = new Vector(-10,0)
+        this.add(fish)
+    }
+}
+
+new Game()
