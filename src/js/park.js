e1a5a08e787130013cda8d0cc6e6ec39988dea39
Eigenaar <youri10jaar@hotmail.com>
2023-06-16 09:20:36 +0200

    wiskenude

--- src/js/park.js
+++ src/js/park.js
@@ -1,13 +1,18 @@
 import {Actor, Engine, Vector, Color, Debug, Physics, Input, Axis, CollisionType, Shape} from "excalibur";
 import {Scene} from "excalibur";
-import {Placeholder} from "./placeholder.js";
+import {Tower} from "./tower.js";
 import {Resources, ResourceLoader} from "./resources.js";
+import {Range} from "./range.js";
+import {Bami} from "./towers/bami.js";
 
 
 let placing = false;
 let placingSprite;
 let int = 0;
-
+let placinge = false;
+let placingSpritee = new Range();
+let activetower;
+let range = new Range();
 export class Park extends Scene {
 
     constructor() {
@@ -16,7 +21,7 @@ export class Park extends Scene {
 
 
     onInitialize(_engine) {
-        placingSprite = new Placeholder();
+        placingSprite = new Bami();
         this.engine.input.pointers.primary.on("down", () => this.mouseInput());
 
         let mapFloor = new Actor();
@@ -28,15 +33,24 @@ export class Park extends Scene {
 
     mouseInput() {
         if (placing) {
-            let newClone = new Placeholder();
+            let newClone = new Tower(this);
             newClone.pos = placingSprite.pos;
             this.add(newClone);
             newClone.checkSelf(int);
+            this.activetower = newClone;
         }
     }
+activeTower(tower){
+        range.kill();
+        this.activetower = tower;
+        console.log(this.activetower);
+        range.pos = this.activetower.pos;
+        range.changeScale(this.activetower.range);
+        console.log(this.activetower.range);
+        this.add(range)
 
+}
     onPreUpdate(engine, delta) {
-
         if (engine.input.keyboard.wasPressed(Input.Keys.B)) {
             placing = !placing;
             console.log(int);
@@ -47,6 +61,23 @@ export class Park extends Scene {
                 placingSprite.kill();
             }
         }
+        if (engine.input.keyboard.wasPressed(Input.Keys.G)) {
+            placinge = !placinge;
+            console.log(int);
+            if (placinge) {
+                this.add(placingSpritee);
+            } else {
+                placingSprite.kill();
+            }
+        }
+        if (engine.input.keyboard.wasPressed(Input.Keys.O)) {
+            this.activetower.updateRange(this.activetower.range -=50);
+            range.changeScale(this.activetower.range)
+        }
+        if (engine.input.keyboard.wasPressed(Input.Keys.P)) {
+            this.activetower.updateRange(this.activetower.range +=50);
+            range.changeScale(this.activetower.range)
+        }
         if (engine.input.keyboard.wasPressed(Input.Keys.N)) {
             int += 1;
             if (int > 1) {
@@ -56,6 +87,9 @@ export class Park extends Scene {
         }
         if (placing) {
             placingSprite.pos = engine.input.pointers.primary.lastWorldPos;
+        }
+        if(placinge){
+            placingSpritee.pos = engine.input.pointers.primary.lastWorldPos;
         }
     }
 }
--- src/js/resources.js
+++ src/js/resources.js
@@ -4,12 +4,16 @@ import start from '../images/start.png';
 import pan from '../images/Bami_Tower.png';
 import kevin from '../images/kevin.png';
 import map1ground from '../images/map-1-ground.png'
+import range from "../images/range.png";
+import bami from "../images/bami.png"
 const Resources = {
     Logo: new ImageSource(logo),
     Start: new ImageSource(start),
     Pan: new ImageSource(pan),
 Kevin: new ImageSource(kevin),
     Map1Ground: new ImageSource(map1ground),
+    Range: new ImageSource(range),
+    Bami: new ImageSource(bami)
 };
 const ResourceLoader = new Loader([
     Resources.Logo,
@@ -17,6 +21,8 @@ const ResourceLoader = new Loader([
     Resources.Pan,
     Resources.Kevin,
     Resources.Map1Ground,
+    Resources.Range,
+    Resources.Bami,
 ]);
 
 export {Resources, ResourceLoader};
